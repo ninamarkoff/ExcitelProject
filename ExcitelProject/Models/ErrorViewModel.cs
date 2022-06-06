e7f280a6381e6c14db8b0a3c03aa3bea82c4bda9
ninanmarkova <ninanmarkova@gmail.com>
2022-06-07 02:56:29 +0300

    Add project files.

---
+++ ExcitelProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExcitelProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
