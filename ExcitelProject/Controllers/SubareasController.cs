146702e93f0274209dbeba7f2f90cf1bc3910997
ninanmarkova <ninanmarkova@gmail.com>
2022-06-07 17:09:18 +0300

    #added Subareas controller;

---
+++ ExcitelProject/Controllers/SubareasController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExcitelProject.Data;
+using ExcitelProject.Models;
+
+namespace ExcitelProject.Controllers
+{
+    public class SubareasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SubareasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Subareas
+        public async Task<IActionResult> Index()
+        {
+              return _context.Subareas != null ? 
+                          View(await _context.Subareas.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Subareas'  is null.");
+        }
+
+        // GET: Subareas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Subareas == null)
+            {
+                return NotFound();
+            }
+
+            var subarea = await _context.Subareas
+                .FirstOrDefaultAsync(m => m.SubareaId == id);
+            if (subarea == null)
+            {
+                return NotFound();
+            }
+
+            return View(subarea);
+        }
+
+        private bool SubareaExists(int id)
+        {
+          return (_context.Subareas?.Any(e => e.SubareaId == id)).GetValueOrDefault();
+        }
+    }
+}
